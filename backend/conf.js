f257872fa21aceb665d7353aa6e3903297c5ea60
LoicBrassart <loic.brassart@gmail.com>
2019-12-10 16:11:29 +0100

    [Too big commit] Added my first functional route =)

--- backend/conf.js
+++ backend/conf.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
-const backendPort = process.env.BACKEND_PORT;
+const mysql = require("mysql");
+
+const backendPort = process.env.BACKEND_PORT || "4200";
+
+const db = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST || "example.org",
+  user: process.env.DB_USER || "bob",
+  password: process.env.DB_PASSWORD || "secret",
+  database: process.env.DB_DATABASE || "my_db"
+});
 
 module.exports = {
-  backendPort
+  backendPort,
+  db
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
-const { backendPort } = require("./conf.js");
+const { backendPort, db } = require("./conf.js");
+
+app.get("/", (req, res) => {
+  db.query(`SELECT 40+2 AS solution`, (err, rows) => {
+    if (err) {
+      console.log("Error on GET / !");
+      console.error(err);
+      res.status(500).send("Sorry, we encountered an internal error.");
+    }
+    res
+      .status(200)
+      .send(`Hello there! The answer is always ${rows[0].solution}`);
+  });
+});
 
 app.listen(backendPort, err => {
   if (err) throw err;
