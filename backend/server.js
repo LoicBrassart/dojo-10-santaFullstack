2d48a6eaac28dec3326ac949500ecba30bc601ea
LoicBrassart <loic.brassart@gmail.com>
2019-12-10 17:20:42 +0100

    Added a route to POST a new gift

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,44 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const { backendPort, db } = require("./conf.js");
 
-app.get("/", (req, res) => {
-  db.query(`SELECT 40+2 AS solution`, (err, rows) => {
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/children", (req, res) => {
+  db.query(
+    `SELECT 
+        child.id AS child_id, firstname, lastname, wisdom, COUNT(gift.id) AS nb_gifts
+    FROM 
+        child
+        INNER JOIN gift ON child.id=gift.id_child
+    GROUP BY child.id`,
+    (err, rows) => {
+      if (err) {
+        console.log("Error on GET /children !");
+        console.error(err);
+        res.status(500).send("Sorry, we encountered an internal error.");
+      }
+      res.status(200).json(rows);
+    }
+  );
+});
+
+app.post("/gifts", (req, res) => {
+  const formData = req.body;
+
+  db.query(`INSERT INTO gift SET ?`, formData, (err, rows) => {
     if (err) {
       console.log("Error on GET / !");
       console.error(err);
       res.status(500).send("Sorry, we encountered an internal error.");
     }
-    res
-      .status(200)
-      .send(`Hello there! The answer is always ${rows[0].solution}`);
+    res.status(200).json(rows);
   });
 });
 
